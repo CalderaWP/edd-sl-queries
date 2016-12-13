77dcf71ba196187fa8024ec8134a2bf9a84f9891
Josh Pollock <Josh@JoshPress.net>
2016-12-12 23:15:39 -0500

    complete user class (creates file trait)

--- src/user.php
+++ src/user.php
@@ -78,23 +78,6 @@ class user {
 
 	}
 
-	/**
-	 * Get a download file for license/ID
-	 *
-	 * @param int $download_id
-	 * @param int $license_id
-	 *
-	 * @return string
-	 */
-	public function get_file_by_license_id( $download_id, $license_id ){
-		$payment_id = \EDD_Software_Licensing::instance()->get_payment_id( $license_id );
-		$payment_key = edd_get_payment_key( $payment_id );
-		$file_key  = get_post_meta( $download_id, '_edd_sl_upgrade_file_key', true );
-		$email       = edd_get_payment_user_email( $payment_id );
 
-		$file = edd_get_download_file_url( $payment_key, $email, $file_key, $download_id );
-
-		return $file;
-	}
 
 }
